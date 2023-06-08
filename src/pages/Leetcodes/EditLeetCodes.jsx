b84b95ef59b1e5234bfc6426fdc306b727b3c053
Harry Teng <43894230+tenghanzhi@users.noreply.github.com>
2023-06-07 22:23:27 -0700

    Completed create/edit leetcode flow

--- src/pages/Leetcodes/EditLeetCodes.jsx
+++ src/pages/Leetcodes/EditLeetCodes.jsx
@@ -1,11 +1,102 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
+import { Typography, Skeleton, message } from "antd";
+import apiMatrix from "../common/apiMatrix";
+import messageMatrix from "../common/messageMatrix";
+import LeetCodesForm from "./LeetCodesForm";
 import LwLayout from "../common/LwLayout";
+import style from "./style/EditLeetCodes.module.css";
 
 const EditLeetCodes = () => {
   const selectedLeetcodeId = useSelector((state) => state.selectedLeetcodeId);
-  const pageContent = <>This is EditLeetCodes Page</>;
-  console.log({ selectedLeetcodeId });
+  const [isEditPageLoading, setIsEditPageLoading] = useState(true);
+  const [fetchedLeetcodeData, setFetchedLeetcodeData] = useState({});
+
+  useEffect(() => {
+    handleGetLeetcodeDataById();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const handleMessage = (type, key, content) => {
+    const messageDuration = 2;
+
+    switch (type) {
+      case "loading": {
+        message.loading({
+          key: key,
+          content: content,
+        });
+        break;
+      }
+      case "success": {
+        message.success({
+          key: key,
+          content: content,
+          duration: messageDuration,
+        });
+        break;
+      }
+      case "error": {
+        message.error({
+          key: key,
+          content: content,
+          duration: messageDuration,
+        });
+        break;
+      }
+      default:
+        return null;
+    }
+  };
+
+  const handleGetLeetcodeDataById = () => {
+    const messageKey = "editPageLoadingMessage";
+    handleMessage("loading", messageKey, messageMatrix.LOADING_MESSAGE_LOADING);
+
+    (async () => {
+      const response = await fetch(
+        apiMatrix.LEET_CODES_GET_BY_ID + selectedLeetcodeId
+      );
+      return response.json();
+    })()
+      .then((response) => {
+        if (response && response.error) {
+          throw new Error(response.error.message);
+        } else {
+        }
+        setFetchedLeetcodeData(response.data.attributes);
+      })
+      .catch((error) => {
+        handleMessage(
+          "error",
+          messageKey,
+          messageMatrix.LOADING_MESSAGE_ERROR + error
+        );
+      })
+      .finally(() => {
+        setIsEditPageLoading(false);
+      });
+  };
+
+  const loadedPageContent = (
+    <>
+      <Typography.Title
+        level={4}
+        className={style.lw_leetcode_edit_leetcode_header}
+      >
+        Edit LeetCode Problem {fetchedLeetcodeData.leetcodeIndex}.{" "}
+        {fetchedLeetcodeData.title}
+      </Typography.Title>
+      <LeetCodesForm isEdit={true} data={fetchedLeetcodeData} />
+    </>
+  );
+
+  const loadingPageContent = <Skeleton />;
+
+  const pageContent = isEditPageLoading
+    ? loadingPageContent
+    : loadedPageContent;
+
   return <LwLayout content={pageContent} />;
 };
 
--- src/pages/common/messageMatrix.js
+++ src/pages/common/messageMatrix.js
@@ -2,6 +2,12 @@ const messageMatrix = {
   LOADING_MESSAGE_LOADING: "Loading most updated data, please wait...",
   LOADING_MESSAGE_SUCCESS: "Successfully loaded data.",
   LOADING_MESSAGE_ERROR: "Failed to load data, please try again. ",
+  UPDATING_MESSAGE_LOADING: "Updating data, please wait...",
+  UPDATING_MESSAGE_SUCCESS: "Successfully updated data.",
+  UPDATING_MESSAGE_ERROR: "Failed to update data, please try again. ",
+  DELETING_MESSAGE_LOADING: "Deleting data, please wait...",
+  DELETING_MESSAGE_SUCCESS: "Successfully deleted data.",
+  DELETING_MESSAGE_ERROR: "Failed to delete data, please try again. ",
   UPLOAD_UPDATED_DATA_MESSAGE_LOADING: "Uploading updated data, please wait...",
   UPLOAD_UPDATED_DATA_MESSAGE_SUCCESS: "Successfully updated data.",
   UPLOAD_UPDATED_DATA_MESSAGE_ERROR:
