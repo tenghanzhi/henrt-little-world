856432f6a8af5e3b74efee57f17c09c045089cee
Harry Teng <43894230+tenghanzhi@users.noreply.github.com>
2023-07-14 00:34:57 -0700

    updated leetcode

---
+++ src/pages/Leetcodes/LeetCodesTopBtns.jsx
@@ -0,0 +1,89 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { Button, Tooltip } from "antd";
+import { PlusOutlined, CodeOutlined } from "@ant-design/icons";
+import categoryMatrix from "../common/categoryMatrix";
+import style from "./style/LeetCodesTopBtns.module.css";
+
+const LeetCodesTopBtns = () => {
+  const navigate = useNavigate();
+  const userInfoData = useSelector((state) => state.userInfoData);
+
+  const handleBtnOnClick = (type) => {
+    switch (type.toLowerCase()) {
+      case "lc en":
+        window.open(
+          "https://leetcode.com/problemset/all/",
+          "_blank",
+          "noopener, noreferrer"
+        );
+        break;
+      case "lc cn":
+        window.open(
+          "https://leetcode.cn/problemset/all/",
+          "_blank",
+          "noopener, noreferrer"
+        );
+        break;
+      case "nc":
+        window.open(
+          "https://neetcode.io/roadmap",
+          "_blank",
+          "noopener, noreferrer"
+        );
+        break;
+      case "create":
+        navigate(`/${categoryMatrix.LEETCODES.toLowerCase()}/createLeetCodes`);
+        break;
+      default:
+        return null;
+    }
+  };
+
+  return (
+    <>
+      <Tooltip
+        title={
+          !userInfoData.jwt ? "Please login with admin account to create" : ""
+        }
+      >
+        <Button
+          type="primary"
+          icon={<PlusOutlined />}
+          onClick={() => handleBtnOnClick("create")}
+          disabled={!userInfoData.jwt}
+          className={style.lw_leetcodes_topBtns_btns}
+        >
+          Create New
+        </Button>
+      </Tooltip>
+      <Button
+        type="default"
+        icon={<CodeOutlined />}
+        onClick={() => handleBtnOnClick("lc cn")}
+        className={style.lw_leetcodes_topBtns_btns}
+      >
+        LeetCode CN
+      </Button>
+      <Button
+        type="default"
+        icon={<CodeOutlined />}
+        onClick={() => handleBtnOnClick("lc en")}
+        className={style.lw_leetcodes_topBtns_btns}
+      >
+        LeetCode EN
+      </Button>
+      <Button
+        type="default"
+        icon={<CodeOutlined />}
+        onClick={() => handleBtnOnClick("nc")}
+        className={style.lw_leetcodes_topBtns_btns}
+      >
+        NeetCode
+      </Button>
+    </>
+  );
+};
+
+export default LeetCodesTopBtns;
--- src/pages/Leetcodes/Leetcodes.jsx
+++ src/pages/Leetcodes/Leetcodes.jsx
@@ -1,41 +1,24 @@
 import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { useNavigate } from "react-router-dom";
-import {
-  message,
-  Space,
-  Button,
-  Input,
-  InputNumber,
-  Select,
-  Tooltip,
-} from "antd";
-import { PlusOutlined, CodeOutlined } from "@ant-design/icons";
+import { message, Space } from "antd";
 import apiMatrix from "../common/apiMatrix";
 import messageMatrix from "../common/messageMatrix";
 import categoryMatrix from "../common/categoryMatrix";
 import LeetCodesTable from "./LeetCodesTable";
 import LeetCodesList from "./LeetCodesList";
+import LeetCodesTopBtns from "./LeetCodesTopBtns";
+import LeetCodesFilter from "./LeetCodesFilter";
 import LwLayout from "../common/LwLayout";
-import {
-  SET_LEETCODE_DATA,
-  SET_LEETCOD_TABLE_FILTER,
-  SET_LEETCOD_TABLE_FILTER_TYPE,
-} from "../../redux/constants";
+import { SET_LEETCODE_DATA } from "../../redux/constants";
 import style from "./style/LeetCodes.module.css";
 
 const LeetCodes = () => {
   const dispatch = useDispatch();
-  const navigate = useNavigate();
-  const userInfoData = useSelector((state) => state.userInfoData);
   const leetcodeTablePagenation = useSelector(
     (state) => state.leetcodeTablePagenation
   );
   const leetcodeTableSorter = useSelector((state) => state.leetcodeTableSorter);
   const leetcodeTableFilter = useSelector((state) => state.leetcodeTableFilter);
-  const leetcodeTableFilterType = useSelector(
-    (state) => state.leetcodeTableFilterType
-  );
 
   useEffect(() => {
     handleGetLeetcodeData();
@@ -46,21 +29,6 @@ const LeetCodes = () => {
     const messageDuration = 2;
 
     switch (type) {
-      case "loading": {
-        message.loading({
-          key: key,
-          content: messageMatrix.LOADING_MESSAGE_LOADING,
-        });
-        break;
-      }
-      case "success": {
-        message.success({
-          key: key,
-          content: messageMatrix.LOADING_MESSAGE_SUCCESS,
-          duration: messageDuration,
-        });
-        break;
-      }
       case "error": {
         message.error({
           key: key,
@@ -69,37 +37,6 @@ const LeetCodes = () => {
         });
         break;
       }
-      default:
-        return null;
-    }
-  };
-
-  const handleBtnOnClick = (type) => {
-    switch (type.toLowerCase()) {
-      case "lc en":
-        window.open(
-          "https://leetcode.com/problemset/all/",
-          "_blank",
-          "noopener, noreferrer"
-        );
-        break;
-      case "lc cn":
-        window.open(
-          "https://leetcode.cn/problemset/all/",
-          "_blank",
-          "noopener, noreferrer"
-        );
-        break;
-      case "nc":
-        window.open(
-          "https://neetcode.io/roadmap",
-          "_blank",
-          "noopener, noreferrer"
-        );
-        break;
-      case "create":
-        navigate(`/${categoryMatrix.LEETCODES.toLowerCase()}/createLeetCodes`);
-        break;
       default:
         return null;
     }
@@ -146,172 +83,11 @@ const LeetCodes = () => {
       });
   };
 
-  const handleSearchPlaceholder = () => {
-    switch (leetcodeTableFilterType) {
-      case "title":
-        return "Input problem title";
-      case "index":
-        return "Input problem LeetCode index";
-      default:
-        return "Please select a search by type to search";
-    }
-  };
-
-  const handleSearchTypeChange = (value) => {
-    dispatch({
-      type: SET_LEETCOD_TABLE_FILTER,
-      payload: {
-        difficulty: null,
-        type: null,
-        leetcodeIndex: null,
-        title: null,
-      },
-    });
-    dispatch({
-      type: SET_LEETCOD_TABLE_FILTER_TYPE,
-      payload: value,
-    });
-  };
-
-  const handleSearchValueChange = (e) => {
-    switch (leetcodeTableFilterType) {
-      case "index":
-        dispatch({
-          type: SET_LEETCOD_TABLE_FILTER,
-          payload: {
-            difficulty: leetcodeTableFilter.difficulty,
-            type: leetcodeTableFilter.type,
-            leetcodeIndex: e,
-            title: leetcodeTableFilter.title,
-          },
-        });
-        break;
-      case "title":
-        dispatch({
-          type: SET_LEETCOD_TABLE_FILTER,
-          payload: {
-            difficulty: leetcodeTableFilter.difficulty,
-            type: leetcodeTableFilter.type,
-            leetcodeIndex: leetcodeTableFilter.leetcodeIndex,
-            title: e.target.value,
-          },
-        });
-        break;
-      default:
-        break;
-    }
-  };
-
-  const handleClearSearchResult = () => {
-    dispatch({
-      type: SET_LEETCOD_TABLE_FILTER,
-      payload: {
-        difficulty: null,
-        type: null,
-        leetcodeIndex: null,
-        title: null,
-      },
-    });
-    dispatch({
-      type: SET_LEETCOD_TABLE_FILTER_TYPE,
-      payload: null,
-    });
-  };
-
-  const searchOptions = [
-    {
-      label: "Search by Index",
-      value: "index",
-    },
-    {
-      label: "Search by Tiitle",
-      value: "title",
-    },
-  ];
-
   const pageContent = (
     <Space direction="vertical" wrap align="start">
       <Space wrap className={style.lw_leetcode_btn_wrapper}>
-        <Tooltip
-          title={
-            !userInfoData.jwt ? "Please login with admin account to create" : ""
-          }
-        >
-          <Button
-            type="primary"
-            icon={<PlusOutlined />}
-            onClick={() => handleBtnOnClick("create")}
-            disabled={!userInfoData.jwt}
-          >
-            Create New
-          </Button>
-        </Tooltip>
-        <Button
-          type="default"
-          icon={<CodeOutlined />}
-          onClick={() => handleBtnOnClick("lc cn")}
-        >
-          LeetCode CN
-        </Button>
-        <Button
-          type="default"
-          icon={<CodeOutlined />}
-          onClick={() => handleBtnOnClick("lc en")}
-        >
-          LeetCode EN
-        </Button>
-        <Button
-          type="default"
-          icon={<CodeOutlined />}
-          onClick={() => handleBtnOnClick("nc")}
-        >
-          NeetCode
-        </Button>
-        <Select
-          className={style.lw_leetcode_search_type_selector}
-          placeholder="Search by"
-          options={searchOptions}
-          onChange={(value) => handleSearchTypeChange(value)}
-          onClear={handleClearSearchResult}
-          value={leetcodeTableFilterType}
-          allowClear
-        />
-        {leetcodeTableFilterType === "index" && (
-          <InputNumber
-            className={style.lw_leetcode_search}
-            placeholder={handleSearchPlaceholder()}
-            onChange={(e) => handleSearchValueChange(e)}
-            disabled={!leetcodeTableFilterType}
-            value={leetcodeTableFilter?.leetcodeIndex}
-            min={0}
-            max={9999}
-            allowClear
-          />
-        )}
-        {leetcodeTableFilterType === "title" && (
-          <Input
-            className={style.lw_leetcode_search}
-            placeholder={handleSearchPlaceholder()}
-            onChange={(e) => handleSearchValueChange(e)}
-            disabled={!leetcodeTableFilterType}
-            value={leetcodeTableFilter?.title}
-            allowClear
-          />
-        )}
-        {leetcodeTableFilterType && (
-          <Button
-            danger
-            onClick={handleClearSearchResult}
-            disabled={
-              (leetcodeTableFilterType === "index" &&
-                !leetcodeTableFilter?.leetcodeIndex) ||
-              (leetcodeTableFilterType === "title" &&
-                !leetcodeTableFilter?.title)
-            }
-          >
-            Clear Results
-          </Button>
-        )}
+        <LeetCodesTopBtns />
+        <LeetCodesFilter />
       </Space>
       <LeetCodesTable className={style.lw_leetcode_table} />
       <LeetCodesList className={style.lw_leetcode_list} />
