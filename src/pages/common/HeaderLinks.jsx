8cdda4542c25a3af1d74102db1266bbc2d671854
Hanzhi Teng <tenghanzhi@outlook.com>
2024-09-29 20:37:38 -0700

    update social links

--- src/pages/common/HeaderLinks.jsx
+++ src/pages/common/HeaderLinks.jsx
@@ -4,10 +4,12 @@ import {
   GithubOutlined,
   LinkedinOutlined,
   WechatOutlined,
+  TwitterOutlined,
 } from "@ant-design/icons";
-import { FaBilibili } from "react-icons/fa6";
+import { FaBilibili, FaYoutube, FaSteam, FaFacebook } from "react-icons/fa6";
 import { BsStackOverflow } from "react-icons/bs";
 import { SiLeetcode } from "react-icons/si";
+import { IoGameController } from "react-icons/io5";
 import style from "./style/HeaderLinks.module.css";
 
 const HeaderLinks = () => {
@@ -41,9 +43,44 @@ const HeaderLinks = () => {
           "noopener, noreferrer"
         );
         break;
+      case "facebook":
+        window.open(
+          "https://www.facebook.com/hanzhi.teng",
+          "_blank",
+          "noopener, noreferrer"
+        );
+        break;
+      case "x":
+        window.open(
+          "https://x.com/tenghanzhi",
+          "_blank",
+          "noopener, noreferrer"
+        );
+        break;
       case "bilibili":
         window.open(
           "https://space.bilibili.com/914572",
+          "_blank",
+          "noopener, noreferrer"
+        );
+        break;
+      case "youtube":
+        window.open(
+          "https://www.youtube.com/@hanzhiteng3449",
+          "_blank",
+          "noopener, noreferrer"
+        );
+        break;
+      case "steam":
+        window.open(
+          "https://steamcommunity.com/id/Legend_T/",
+          "_blank",
+          "noopener, noreferrer"
+        );
+        break;
+      case "wows":
+        window.open(
+          "https://profile.worldofwarships.com/statistics/1031221943",
           "_blank",
           "noopener, noreferrer"
         );
@@ -55,6 +92,22 @@ const HeaderLinks = () => {
 
   return (
     <div className={style.lw_header_links_wrapper}>
+      <Tooltip placement="bottom" title={"Check Henry's Steam"}>
+        <Button
+          className={style.lw_header_links}
+          type="link"
+          icon={<FaSteam />}
+          onClick={() => onRightBtnsClick("steam")}
+        />
+      </Tooltip>
+      <Tooltip placement="bottom" title={"Check Henry's World of Warships"}>
+        <Button
+          className={style.lw_header_links}
+          type="link"
+          icon={<IoGameController />}
+          onClick={() => onRightBtnsClick("wows")}
+        />
+      </Tooltip>
       <Tooltip placement="bottom" title={"Check Henry's LeetCode"}>
         <Button
           className={style.lw_header_links}
@@ -87,12 +140,36 @@ const HeaderLinks = () => {
           onClick={() => onRightBtnsClick("linkedin")}
         />
       </Tooltip>
+      <Tooltip placement="bottom" title={"Check Henry's YouTube"}>
+        <Button
+          className={style.lw_header_links}
+          type="link"
+          icon={<FaYoutube />}
+          onClick={() => onRightBtnsClick("youtube")}
+        />
+      </Tooltip>
       <Tooltip placement="bottom" title={"Check Henry's Bilibili"}>
         <Button
           className={style.lw_header_links}
           type="link"
           icon={<FaBilibili />}
           onClick={() => onRightBtnsClick("bilibili")}
+        />
+      </Tooltip>
+      <Tooltip placement="bottom" title={"Check Henry's X"}>
+        <Button
+          className={style.lw_header_links}
+          type="link"
+          icon={<TwitterOutlined />}
+          onClick={() => onRightBtnsClick("x")}
+        />
+      </Tooltip>
+      <Tooltip placement="bottom" title={"Check Henry's FaceBook"}>
+        <Button
+          className={style.lw_header_links}
+          type="link"
+          icon={<FaFacebook />}
+          onClick={() => onRightBtnsClick("facebook")}
         />
       </Tooltip>
       <Tooltip
